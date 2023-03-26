6a70b9c4332f18c866b27f29a00361b75b12f111
Faris Ilham Noormandiri <secondlast2102@gmail.com>
2023-03-26 19:00:09 +0700

    Progress Map SPT

--- src/components/MapSPT/index.tsx
+++ src/components/MapSPT/index.tsx
@@ -5,11 +5,7 @@ import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility
 import { useQuery } from "react-query";
 import { getLand } from "@/services/mapService";
 import { LandLocation } from "@/services/mapService";
-import {
-  BaseAreaLocation,
-  getAreaLocation,
-  BaseMapResponse,
-} from "@/services/mapService";
+import { BaseMapResponse } from "@/services/mapService";
 import { LatLngExpression } from "leaflet";
 import { useState, useEffect } from "react";
 
@@ -18,38 +14,35 @@ const param: LandLocation = {
 };
 
 export default function Index() {
-  const purpleOptions = { color: "purple" };
-  const [coordinates, setCoordinates] = useState<number[][][] | undefined>([]);
+  const [coordinates, setCoordinates] = useState<any>([]);
 
   const { data: dataLand, isFetched: isFetchedLand } =
     useQuery<BaseMapResponse>("getLandLocation", () => getLand(param));
 
-  const { data: dataArea, isFetched: isFetchedArea } =
-    useQuery<BaseAreaLocation>("getAreaLocation", getAreaLocation);
-
   useEffect(() => {
     if (isFetchedLand) {
       const _coordinates = dataLand?.data.map((landLocation) => {
-        const temp =
-          landLocation.geometryLandLocation.geometry.geometry.coordinates?.[0];
-        temp?.map((location) => location.reverse());
-        return temp;
+        return landLocation.geometryLandLocation.geometry.geometry.coordinates.map(
+          (coorMap) => {
+            let temp = coorMap.reverse();
+            temp?.map((location) => location.reverse());
+            return temp;
+          }
+        );
       });
-      console.log(_coordinates);
       setCoordinates(_coordinates);
     }
   }, [isFetchedLand]);
 
   console.log(coordinates);
 
-  // if (isFetchedLand) {
-  //   console.log("Ini Land");
-  //   console.log(dataLand);
-  //   dataLand?.data.map((landLocation) =>
-  //     console.log(
-  //       landLocation.geometryLandLocation.geometry.geometry.coordinates[0]
-  //     )
-  //   );
+  // Define an object that maps land suitability class to colors
+  const landSuitabilityColors: { [key: string]: any } = {
+    4: { fillColor: "#33CC33", color: "#fff" },
+    3: { fillColor: "#FFFF00", color: "#fff" },
+    2: { fillColor: "#FF6600", color: "#fff" },
+    1: { fillColor: "#FF0000", color: "#fff" },
+  };
 
   return (
     <div>
@@ -64,15 +57,35 @@ export default function Index() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {isFetchedLand && (
-          <Polygon
-            pathOptions={purpleOptions}
-            positions={coordinates as LatLngExpression[][]}
-          />
-        )}
-        <Marker position={[-7.503168958076711, 110.18966941496906]}>
-          <Popup>{dataArea?.data?.[0][1]?.bpsCode}</Popup>
-        </Marker>
+        {isFetchedLand &&
+          coordinates.map((polygonCoords: any, index: any) => {
+            // Get the land suitability class of the polygon
+            const landSuitabilityClass =
+              dataLand?.data[index]?.observations[0]?.landSuitabilityClass;
+
+            // Set the fill color and stroke color based on the land suitability class
+            const { fillColor, color } = landSuitabilityColors[2] || {
+              fillColor: "#000000",
+              color: "#000000",
+            };
+
+            // const { fillColor, color } = landSuitabilityColors[
+            //   landSuitabilityClass
+            // ] || {
+            //   fillColor: "#000000",
+            //   color: "#000000",
+            // };
+
+            return (
+              <Polygon
+                key={index}
+                pathOptions={{ fillColor, color, weight: 1, fillOpacity: 1 }}
+                positions={polygonCoords}
+              >
+                <Popup>tes</Popup>
+              </Polygon>
+            );
+          })}
       </MapContainer>
     </div>
   );
